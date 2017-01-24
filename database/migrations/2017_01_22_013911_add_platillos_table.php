38613403d9f23dae35ec329a6ad9775bf2c05582
Richard Fernandez <homogeneamarketing@gmail.com>
2017-01-23 18:34:24 -0800

    creacion de las migraciones para la base de datos solo tablas base

---
+++ database/migrations/2017_01_22_013911_add_platillos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPlatillosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('platillos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre',150);
+            $table->string('receta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('platillos');
+    }
+}
